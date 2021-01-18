6126bf80bb2ab1646c5844d4df63e17f35332ace
huangzhixue <1104428690@qq.com>
2021-01-18 18:32:14 +0800

    剑指 Offer 13. 机器人的运动范围

--- src/offer/Offer13.java
+++ src/offer/Offer13.java
@@ -17,6 +17,12 @@ package offer;
  * 0 <= k <= 20
  *
  * 数位之和这个坑..
+ * 思路1：递归
+ *
+ * 思路2：广度优先遍历BFS(按树的层级遍历，一般使用队列实现LinkedList)
+ * 利用队列的先进先出设计，先将头部入队，然后出队时将左右子树入队(左右指数属于2层)。同样道理，左/右子树出队，又会将3层的左右子树入队....
+ *
+ * 思路3：深度优先遍历DFS(一遍从左树深入到底，然后回调时继续遍历。一般采用栈回调时进行)
  *
  *
  * @author ：HUANG ZHI XUE
@@ -30,7 +36,7 @@ public class Offer13 {
 
     private int dnf(int m, int n, int i, int j, int k, int[][] arr) {
         // 递归结束条件（行+列>k 、不能超出空格外）
-        if (i < 0 || i > m - 1 || j < 0 || j > n - 1 || i % 10 + j % 10 > k || arr[i][j] == 1) {
+        if (i < 0 || i > m - 1 || j < 0 || j > n - 1 || sum(i, j) > k || arr[i][j] == 1) {
             return 0;
         }
         // 记录是否走过
@@ -45,4 +51,22 @@ public class Offer13 {
         return count + 1;
     }
 
+    /**
+     * 计算数位之和
+     * @param i 横坐标
+     * @param j 纵坐标
+     * @return 数位之和
+     */
+    public int sum(int i, int j) {
+        int sum = 0;
+        while (i != 0) {
+            sum += i % 10;
+            i /= 10;
+        }
+        while (j != 0) {
+            sum += j % 10;
+            j /= 10;
+        }
+        return sum;
+    }
 }

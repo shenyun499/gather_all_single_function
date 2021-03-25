0b7f74ba09a965f35a204ae7bd984ea3b46613af
huangzhixue <1104428690@qq.com>
2021-03-25 17:11:39 +0800

    单例模式代码实现

--- src/musthandwritecode/Singleton.java
+++ src/musthandwritecode/Singleton.java
@@ -7,4 +7,18 @@ package musthandwritecode;
  * @date ：Create in 2021-03-11
  */
 public class Singleton {
+    private volatile static Singleton singleton = null;
+
+    private Singleton(){}
+
+    public static Singleton getInstance() {
+        if (singleton == null) {
+            synchronized (Singleton.class) {
+                if (singleton == null) {
+                    singleton = new Singleton();
+                }
+            }
+        }
+        return singleton;
+    }
 }

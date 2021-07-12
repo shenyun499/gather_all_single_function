f6a8dd1f2987dc569de04a442938238abbdd10bd
huangzhixue <1104428690@qq.com>
2021-07-12 17:29:56 +0800

    引入 swagger3.0

---
+++ src/main/java/pers/xue/skills/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package pers.xue.skills.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+/**
+ * @auther huangzhixue
+ * @data 2021/7/12 4:18 下午
+ * @Description swagger 配置
+ */
+@Configuration
+@EnableSwagger2WebMvc
+public class SwaggerConfig {
+    @Bean
+    public Docket createApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("pers.xue.skills.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * 设置swagger的一些信息
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("gather_all_single_function-Swagger2")
+                .description("Restful API接口")
+                .version("1.0.0")
+                .build();
+    }
+}

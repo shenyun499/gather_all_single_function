0e2a5fa1c13a0774bb773095bc96b677bc38c14e
huangzhixue <1104428690@qq.com>
2021-07-14 16:09:42 +0800

    加入 srping-factories 指定扫描启动类路径外的bean

--- src/main/java/pers/xue/skills/App.java
+++ src/main/java/pers/xue/skills/App.java
@@ -3,17 +3,14 @@ package pers.xue.skills;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Hello world!
  *
  */
-@RestController
 @SpringBootApplication
 @EntityScan
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
--- src/main/java/pers/xue/skills/config/SwaggerConfig.java
+++ src/main/java/pers/xue/skills/config/SwaggerConfig.java
@@ -2,13 +2,13 @@ package pers.xue.skills.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 /**
  * @auther huangzhixue
@@ -16,7 +16,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
  * @Description swagger 配置
  */
 @Configuration
-@EnableSwagger2WebMvc
 public class SwaggerConfig {
     @Bean
     public Docket createApi() {

79da25c938d926ee905656da5cb8f58275c48e7c
huangzhixue <1104428690@qq.com>
2021-09-09 16:56:07 +0800

    加入 logback

--- src/main/java/pers/xue/skills/App.java
+++ src/main/java/pers/xue/skills/App.java
@@ -3,6 +3,7 @@ package pers.xue.skills;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
  * Hello world!
@@ -14,7 +15,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(App.class, args);
+        SpringApplication app = new SpringApplication(App.class);
+        ConfigurableEnvironment environment = app.run(args).getEnvironment();
+        System.out.println("s");
     }
 
     // Spring Validation默认会校验完所有字段，然后才抛出异常。可以通过一些简单的配置，开启Fali Fast模式，一旦校验失败就立即返回。

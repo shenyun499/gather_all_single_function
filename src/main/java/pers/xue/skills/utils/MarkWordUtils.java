cd9e16a849984d89acd7fc64e66bd33debd105c1
huangzhixue <1104428690@qq.com>
2021-11-28 21:03:33 +0800

    -XX:BiasedLockingBulkRebiasThreshold = 20   // 默认偏向锁批量重偏向阈值

--- src/main/java/pers/xue/skills/utils/MarkWordUtils.java
+++ src/main/java/pers/xue/skills/utils/MarkWordUtils.java
@@ -14,6 +14,7 @@ import java.util.concurrent.locks.LockSupport;
 public class MarkWordUtils {
 
     public static void main(String[] args) throws InterruptedException {
+
         Thread.sleep(5000);
         MarkWordUtils markWordUtils = new MarkWordUtils();
 
@@ -31,11 +32,12 @@ public class MarkWordUtils {
             }
         });
         t1.start();
+        // 线程1不在运行时，再去开启线程2
         while (t1.isAlive()){};
         t2.start();
     }
 
-    public static void a2(String[] args) throws InterruptedException {
+    public static void main2(String[] args) throws InterruptedException {
         Thread.sleep(5000);
 // 首先我们创建一个list，来存放锁对象
 
@@ -68,6 +70,9 @@ public class MarkWordUtils {
 
 // 线程2
 
+        // -XX:BiasedLockingBulkRebiasThreshold = 20   // 默认偏向锁批量重偏向阈值
+        // 意思就是重偏向是开启的，但是得根据频率决定，就是发现有19次那么频繁的进行重偏向，但是前面19次是到了轻量锁，那么第20次开始，就重偏向到偏向锁了。。
+        // 持有偏向锁的线程ID和其他信息。这个线程ID并不是JVM分配的线程ID号，和Java Thread中的ID是两个概念。 偏向锁才有，固定54位
         new Thread(() -> {
             for (int i = 0; i < 30; i++) {
                 MarkWordUtils testBiasedLocking = list.get(i);

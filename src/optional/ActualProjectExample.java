592b8b9a8fe8ce26cf924e2dd98e72dca611691a
huangzhixue <1104428690@qq.com>
2022-01-25 14:21:03 +0800

    Add actual project example for deal with null case

---
+++ src/optional/ActualProjectExample.java
@@ -0,0 +1,129 @@
+package optional;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Optional;
+
+/**
+ * @author huangzhixue
+ * @date 2022/1/25 12:22 下午
+ * @Description
+ */
+public class ActualProjectExample {
+
+    public void test1() {
+        User user = User.builder().name("神韵").address(User.Address.builder().city("河源").build()).build();
+        //例一  以前写法
+        if (user != null) {
+            dosomething(user);
+        }
+
+        // JAVA8写法
+        Optional.ofNullable(user).ifPresent(u -> { dosomething(u);});
+    }
+
+    public void test2() {
+        User user = User.builder().name("神韵").address(User.Address.builder().city("河源").build()).build();
+        //例二  以前写法
+        String city_old_02 = getCity_old_02(user);
+
+        // JAVA8写法
+        String city_java8_02 = getCity_java8_02(user);
+    }
+
+    public void test3() {
+        User user = User.builder().name("神韵").address(User.Address.builder().city("河源").build()).build();
+        //例三  以前写法
+        User user_old_03 = getUser_old_03(user);
+
+        // JAVA8写法
+        User user_java8_03 = getUser_java8_03(user);
+    }
+
+    /**
+     * 在函数方法中,以前写法
+     * @param user
+     * @return
+     */
+    public static String getCity_old_02(User user) {
+        if (user != null) {
+            if (user.getAddress() != null) {
+                User.Address address = user.getAddress();
+                if (address.getCity() != null) {
+                    return address.getCity();
+                }
+            }
+        }
+        throw new RuntimeException("取值错误");
+    }
+
+    /**
+     * JAVA8写法
+     * @param user
+     * @return
+     */
+    public String getCity_java8_02(User user) {
+        return Optional.ofNullable(user)
+                .map(u -> u.getAddress())
+                .map(a -> a.getCity())
+                .orElseThrow(() -> new RuntimeException("取指错误"));
+    }
+
+
+    /**
+     * 以前写法
+     * @param user
+     * @return
+     */
+    public User getUser_old_03(User user) {
+        if (user != null) {
+            String name = user.getName();
+            if ("zhangsan".equals(name)) {
+                return user;
+            }
+        } else {
+            user = new User();
+            user.setName("zhangsan");
+            return user;
+        }
+        return null;
+    }
+
+    /**
+     * java8写法
+     * @param user
+     * @return
+     */
+    public User getUser_java8_03(User user){
+        return Optional.ofNullable(user)
+                .filter(u -> "zhangsan".equals(u.getName()))
+                .orElseGet(() -> {
+                    User user1 = new User();
+                    user1.setName("zhangsan");
+                    return user1;
+                });
+    }
+
+
+    @Setter
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    static class User {
+        private String name;
+        private Address address;
+        @Setter
+        @Getter
+        @Builder
+        static class Address {
+            private String city;
+        }
+    }
+
+    private void dosomething(Object u) {
+        // do anything
+    }
+}
